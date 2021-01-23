94d87df77e1e5bae094366c5abf03fe74b155d75
Connor <connorpuhala@gmail.com>
2021-01-23 10:28:50 -0800

    changed sqlite to postgres

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'PersonalPortfolio',                      
+        'USER': 'postgres',
+        'PASSWORD': 'SunKing22',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
